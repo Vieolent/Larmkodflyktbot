05cf0b8e38c5c11459488e37b384e4917dbf2e77
Miranda <miranda.l.bache@gmail.com>
2025-05-08 15:42:23 +0200

    Add files via upload

--- larmkodflyktbot.ino
+++ larmkodflyktbot.ino
@@ -1,20 +1,31 @@
+/*
+Namn: Miranda Bache
+Projekt: Larmkodflyktbot
+Senast uppdaterad: 2025-05-08
+*/
+
+
+//Bibliotek för servo
 #include <Servo.h>
+//Bibliotek för keypad
 #include <Keypad.h>
 
+//konstanter definiteras, första två är pins och andra två definerar storleken på min keypad
 const int hoger = 8;
 const int vanster = 9;
 const byte ROWS = 4;
 const byte COLS = 3;
 
-int index = 0;
-int code[] = { 7, 2, 5 };
+//variabler defineras
+int index = 0; 
+int code[] = { 7, 2, 5 }; //korrekt kod
 int inputCode[3] = {};
 int code_Attempts = 0;
-float duration, distance1, distance2, average1, average2;
+float duration, distance1, distance2, average1, average2; //variabler för sensordata
 float array1[10] = {};
 float array2[10] = {};
-float temp = 0;
 
+//keypadens layout
 char hexaKeys[ROWS][COLS] = {
   { '1', '2', '3' },
   { '4', '5', '6' },
@@ -22,36 +33,44 @@ char hexaKeys[ROWS][COLS] = {
   { '*', '0', '#' }
 };
 
+//pins för keypad
 byte rowPins[ROWS] = { 10, 0, 5, 11 };
 byte colPins[COLS] = { 12, 13, 4 };
 
+//keypad definerars
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 Servo myservo;
 
 
 void setup() {
-  pinMode(6, OUTPUT);
+  //servo och seriellmonitor sätts igång i rätt läge
   myservo.attach(3);
   Serial.begin(9600);
+  myservo.write(0);
+
+  //fler pins defineras
+  pinMode(6, OUTPUT);
   pinMode(A0, INPUT);
   pinMode(7, OUTPUT);
   pinMode(A2, INPUT);
   pinMode(2, OUTPUT);
   pinMode(hoger, OUTPUT);
   pinMode(vanster, OUTPUT);
-  myservo.write(0);
+
 }
 
 void loop() {
-
+  //skriver ner vad för key har tryckts på
   char customKey = customKeypad.getKey();
 
+//lägger till tangenten som tryckts på i en liten array
   if (customKey) {
     inputCode[index] = customKey - '0';
     index++;
   }
 
+//räknar ut ifall man har skrivit in rätt kod
   if (index == 3) {
     index = 0;
     if (code[0] == inputCode[0] && code[1] == inputCode[1] && code[2] == inputCode[2]) {
@@ -65,30 +84,32 @@ void loop() {
     }
   }
 
-
+//om man ta för många försök på sig att gissa koden börjar den köra
   while (code_Attempts > 3) {
     
-
+    //sensordata räknas ut
     distance1 = getDistance1();
     distance2 = getDistance2();
     average1 = average(distance1, array1);
     average2 = average(distance2, array2);
 
-    //Serial.print(400);
-    //Serial.print(" ");
+    //sensordata skrivs upp i ett format som fungerar med seriell plottern
     Serial.print(average1);
     Serial.print(" ");
     Serial.print(average2);
     Serial.println(" ");
 
+    //utgångsläget defineras som att bilen kör framåt
     digitalWrite(hoger, HIGH);
     digitalWrite(vanster, HIGH);
 
+    //om bilen är allt för nära något står den still
     if (average1 < 15 && average2 < 15){
       digitalWrite(vanster, LOW); 
       digitalWrite(hoger, LOW);
     }
 
+    // om bilen är relativt nära något kör den åt det håll där det är mest troligt att den kommer kunna undvika hindret
     else if (average1 < 50 && average2 < 50 && average1 < average2) {
       digitalWrite(vanster, LOW); 
       digitalWrite(hoger, HIGH);
@@ -98,6 +119,7 @@ void loop() {
       digitalWrite(hoger, LOW);
     }
 
+    //om bara en av sensorerna tycker den kommer krasha svänger den 
     else if (average2 < 50) {
       digitalWrite(vanster, LOW);
     }
--- larmkodflyktbot_funktioner.ino
+++ larmkodflyktbot_funktioner.ino
@@ -1,4 +1,7 @@
- float average(float value, float array[]){
+ //funktion som jämnar ut sensordata genom att ta medelvärdet av de senaste 10 värdena
+ //Input: float med rå sensorvärde och array där den kan skriva ner och ta medelvärdet på sensorvärdena
+ //Output: Flyttal som är utjämnade
+float average(float value, float array[]){
   array[9] = value;
    float sum = 0;
   for(int i = 0; i < 10; i++){
@@ -8,6 +11,9 @@
   return sum/10;
 }
 
+//funktion som tar in sensordata från högra sensoren
+//Input: inga
+//Output: rå sensordata
 float getDistance2(){
 	digitalWrite(2, LOW);  
 	delayMicroseconds(2);  
@@ -23,6 +29,9 @@ float getDistance2(){
   return distance2;
 }
 
+//funktion som tar in sensordata från vänstra sensoren
+//Input: inga
+//Output: rå sensordata
 float getDistance1(){
 	digitalWrite(7, LOW);  
 	delayMicroseconds(2);  
